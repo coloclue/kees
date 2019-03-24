e353ca9a06bca19147dcc6f607290cfd593033f3
Cybertinus <coloclue@cybertinus.nl>
2019-03-24 11:46:08 +0100

    Add support for other URLs where the RPKI ROA JSON can be found

--- generate-peer-config.sh
+++ generate-peer-config.sh
@@ -8,9 +8,16 @@ if [ "$(python -c "import yaml,sys;a = yaml.safe_load(sys.stdin); print a['rpki'
     if [ ! -d /opt/routefilters/rpki ] ; then
         mkdir /opt/routefilters/rpki
     fi
-    
-    rtrsub --afi ipv4 < ./templates/bird-rpki.j2 > /opt/routefilters/rpki/rpki-ipv4.conf
-    rtrsub --afi ipv6 < ./templates/bird-rpki.j2 > /opt/routefilters/rpki/rpki-ipv6.conf
+
+    rpki_json_url="$(python -c "import yaml,sys; generic_yaml = yaml.safe_load(sys.stdin); print generic_yaml['rpki_json_url'];" < vars/generic.yml 2>/dev/null)"
+    if [ "${?}" -ne 0 -o -z "${rpki_json_url}" ] ; then
+        rtrsub_options=''
+    else
+        rtrsub_options="-c ${rpki_json_url}"
+    fi
+
+    rtrsub --afi ipv4 $rtrsub_options < ./templates/bird-rpki.j2 > /opt/routefilters/rpki/rpki-ipv4.conf
+    rtrsub --afi ipv6 $rtrsub_options < ./templates/bird-rpki.j2 > /opt/routefilters/rpki/rpki-ipv6.conf
 
     ./gentool -4 -y vars/generic.yml -t templates/rpkiwhitelist.j2 -o /opt/routefilters/rpki/rpkiwhitelist-ipv4.conf
     ./gentool -6 -y vars/generic.yml -t templates/rpkiwhitelist.j2 -o /opt/routefilters/rpki/rpkiwhitelist-ipv6.conf
