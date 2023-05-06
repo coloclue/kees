a4f99dde95162079eef0418d70c5ae70fdee8673
Rogier Krieger <rkrieger@users.noreply.github.com>
2023-05-06 17:40:01 +0200

    Let users configure build/staging dirs. Fixes #15. (#47)
    Introduces two configuration settings for vars/generic.yml:
    * builddir: where `generate-peer-config.sh` and `peering_filters` output their prefix lists and filters
    * stagedir: from which `update-routers.sh` pushes final configurations to individual routers
    
    Using environment variables BUILDDIR and STAGEDIR will override the configured values. If neither configuration or environment settings are present, the scripts revert to their former hard-coded defaults.
    
    To allow shell scripts to use the YAML configuration, they now dot-source functions.sh. The getconfig function reads YAML file variables. I'm aware this is fragile at best, but it "Works for me".
    
    Test runs work fine on my platform, using both configured and environment settings.
    
    Finally, .gitignore also lets users place a separate virtualenv underneath and not be bothered.

--- update-routers.sh
+++ update-routers.sh
@@ -12,61 +12,69 @@ fi
 # 20230414 Temp move of eunetworks-2 to the back, because of the VPP tests
 routers='dcg-1.router.nl.coloclue.net dcg-2.router.nl.coloclue.net eunetworks-3.router.nl.coloclue.net eunetworks-2.router.nl.coloclue.net'
 
+. functions.sh
+
+# Get output/staging dirs from Kees configuration
+BUILDDIR=${BUILDDIR:-`getconfig 'builddir' '/opt/routefilters'`}
+echo Building in \'${BUILDDIR}\'
+STAGEDIR=${STAGEDIR:-`getconfig 'stagedir' '/opt/router-staging'`}
+echo Staging files in \'${STAGEDIR}\'
+
 # generate peer configs
 ./peering_filters "${arguments}"
 
 for router in ${routers}; do
-    rm -rf /opt/router-staging/${router}
-    mkdir -p /opt/router-staging/${router}
+    rm -rf ${STAGEDIR}/${router}
+    mkdir -p ${STAGEDIR}/${router}
 
-    ./gentool -y vars/generic.yml -t templates/envvars.j2 -o /opt/router-staging/${router}/envvars
-    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/ebgp_state.j2 -o /opt/router-staging/${router}/ebgp_state.conf
+    ./gentool -y vars/generic.yml -t templates/envvars.j2 -o ${STAGEDIR}/${router}/envvars
+    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/ebgp_state.j2 -o ${STAGEDIR}/${router}/ebgp_state.conf
 
-    ./gentool -4 -y vars/generic.yml vars/${router}.yml -t templates/header.j2 -o /opt/router-staging/${router}/header-ipv4.conf
-    ./gentool -6 -y vars/generic.yml vars/${router}.yml -t templates/header.j2 -o /opt/router-staging/${router}/header-ipv6.conf
-    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/bfd.j2 -o /opt/router-staging/${router}/bfd.conf
-    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/ospf.j2 -o /opt/router-staging/${router}/ospf.conf
+    ./gentool -4 -y vars/generic.yml vars/${router}.yml -t templates/header.j2 -o ${STAGEDIR}/${router}/header-ipv4.conf
+    ./gentool -6 -y vars/generic.yml vars/${router}.yml -t templates/header.j2 -o ${STAGEDIR}/${router}/header-ipv6.conf
+    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/bfd.j2 -o ${STAGEDIR}/${router}/bfd.conf
+    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/ospf.j2 -o ${STAGEDIR}/${router}/ospf.conf
 
-    ./gentool -4 -y vars/generic.yml vars/${router}.yml -t templates/ibgp.j2 -o /opt/router-staging/${router}/ibgp-ipv4.conf
-    ./gentool -6 -y vars/generic.yml vars/${router}.yml -t templates/ibgp.j2 -o /opt/router-staging/${router}/ibgp-ipv6.conf
+    ./gentool -4 -y vars/generic.yml vars/${router}.yml -t templates/ibgp.j2 -o ${STAGEDIR}/${router}/ibgp-ipv4.conf
+    ./gentool -6 -y vars/generic.yml vars/${router}.yml -t templates/ibgp.j2 -o ${STAGEDIR}/${router}/ibgp-ipv6.conf
 
-    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/interfaces.j2 -o /opt/router-staging/${router}/interfaces.conf
+    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/interfaces.j2 -o ${STAGEDIR}/${router}/interfaces.conf
 
-    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/generic_filters.j2 -o /opt/router-staging/${router}/generic_filters.conf
+    ./gentool -y vars/generic.yml vars/${router}.yml -t templates/generic_filters.j2 -o ${STAGEDIR}/${router}/generic_filters.conf
 
-    ./gentool -4 -y vars/generic.yml vars/${router}.yml -t templates/afi_specific_filters.j2 -o /opt/router-staging/${router}/ipv4_filters.conf
-    ./gentool -6 -y vars/generic.yml vars/${router}.yml -t templates/afi_specific_filters.j2 -o /opt/router-staging/${router}/ipv6_filters.conf
+    ./gentool -4 -y vars/generic.yml vars/${router}.yml -t templates/afi_specific_filters.j2 -o ${STAGEDIR}/${router}/ipv4_filters.conf
+    ./gentool -6 -y vars/generic.yml vars/${router}.yml -t templates/afi_specific_filters.j2 -o ${STAGEDIR}/${router}/ipv6_filters.conf
 
-    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/members_bgp.yml -t templates/members_bgp.j2 -o /opt/router-staging/${router}/members_bgp-ipv4.conf
-    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/members_bgp.yml -t templates/members_bgp.j2 -o /opt/router-staging/${router}/members_bgp-ipv6.conf
+    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/members_bgp.yml -t templates/members_bgp.j2 -o ${STAGEDIR}/${router}/members_bgp-ipv4.conf
+    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/members_bgp.yml -t templates/members_bgp.j2 -o ${STAGEDIR}/${router}/members_bgp-ipv6.conf
 
-    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/transit.yml -t templates/transit.j2 -o /opt/router-staging/${router}/transit-ipv4.conf
-    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/transit.yml -t templates/transit.j2 -o /opt/router-staging/${router}/transit-ipv6.conf
+    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/transit.yml -t templates/transit.j2 -o ${STAGEDIR}/${router}/transit-ipv4.conf
+    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/transit.yml -t templates/transit.j2 -o ${STAGEDIR}/${router}/transit-ipv6.conf
 
-    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/scrubbers.j2 -o /opt/router-staging/${router}/scrubber-ipv4.conf
-    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/scrubbers.j2 -o /opt/router-staging/${router}/scrubber-ipv6.conf
-    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/via_scrubbers_afi.j2 -o /opt/router-staging/${router}/via_scrubbers_ipv4.conf
-    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/via_scrubbers_afi.j2 -o /opt/router-staging/${router}/via_scrubbers_ipv6.conf
+    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/scrubbers.j2 -o ${STAGEDIR}/${router}/scrubber-ipv4.conf
+    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/scrubbers.j2 -o ${STAGEDIR}/${router}/scrubber-ipv6.conf
+    ./gentool -4 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/via_scrubbers_afi.j2 -o ${STAGEDIR}/${router}/via_scrubbers_ipv4.conf
+    ./gentool -6 -y vars/generic.yml vars/${router}.yml vars/scrubbers.yml -t templates/via_scrubbers_afi.j2 -o ${STAGEDIR}/${router}/via_scrubbers_ipv6.conf
 
     # DCG specific stuff
     if [ "${router}" == "dcg-1.router.nl.coloclue.net" ] || [ "${router}" == "dcg-2.router.nl.coloclue.net" ]; then
-        ./gentool -4 -t templates/static_routes.j2 -y vars/statics-dcg.yml -o /opt/router-staging/${router}/static_routes-ipv4.conf
-        ./gentool -6 -t templates/static_routes.j2 -y vars/statics-dcg.yml -o /opt/router-staging/${router}/static_routes-ipv6.conf
+        ./gentool -4 -t templates/static_routes.j2 -y vars/statics-dcg.yml -o ${STAGEDIR}/${router}/static_routes-ipv4.conf
+        ./gentool -6 -t templates/static_routes.j2 -y vars/statics-dcg.yml -o ${STAGEDIR}/${router}/static_routes-ipv6.conf
     # EUNetworks specific stuff
     elif [ "${router}" == "eunetworks-2.router.nl.coloclue.net" ] || [ "${router}" == "eunetworks-3.router.nl.coloclue.net" ]; then
-        ./gentool -4 -t templates/static_routes.j2 -y vars/statics-eunetworks.yml -o /opt/router-staging/${router}/static_routes-ipv4.conf
-        ./gentool -6 -t templates/static_routes.j2 -y vars/statics-eunetworks.yml -o /opt/router-staging/${router}/static_routes-ipv6.conf
+        ./gentool -4 -t templates/static_routes.j2 -y vars/statics-eunetworks.yml -o ${STAGEDIR}/${router}/static_routes-ipv4.conf
+        ./gentool -6 -t templates/static_routes.j2 -y vars/statics-eunetworks.yml -o ${STAGEDIR}/${router}/static_routes-ipv6.conf
     fi
 
-    rsync -av blobs/${router}/ /opt/router-staging/${router}/
-    rsync -av /opt/routefilters/rpki/ /opt/router-staging/${router}/rpki/
+    rsync -av blobs/${router}/ ${STAGEDIR}/${router}/
+    rsync -av ${BUILDDIR}/rpki/ ${STAGEDIR}/${router}/rpki/
 
-    rsync -av --delete /opt/routefilters/*bird* /opt/router-staging/${router}/peerings/
-    rsync -av --delete /opt/routefilters/${router}.ipv4.config /opt/router-staging/${router}/peerings/peers.ipv4.conf
-    rsync -av --delete /opt/routefilters/${router}.ipv6.config /opt/router-staging/${router}/peerings/peers.ipv6.conf
+    rsync -av --delete ${BUILDDIR}/*bird* ${STAGEDIR}/${router}/peerings/
+    rsync -av --delete ${BUILDDIR}/${router}.ipv4.config ${STAGEDIR}/${router}/peerings/peers.ipv4.conf
+    rsync -av --delete ${BUILDDIR}/${router}.ipv6.config ${STAGEDIR}/${router}/peerings/peers.ipv6.conf
 
-    TZ=Etc/UTC date '+# Created: %a, %d %b %Y %T %z' >>  /opt/router-staging/${router}/bird.conf
-    TZ=Etc/UTC date '+# Created: %a, %d %b %Y %T %z' >>  /opt/router-staging/${router}/bird6.conf
+    TZ=Etc/UTC date '+# Created: %a, %d %b %Y %T %z' >>  ${STAGEDIR}/${router}/bird.conf
+    TZ=Etc/UTC date '+# Created: %a, %d %b %Y %T %z' >>  ${STAGEDIR}/${router}/bird6.conf
 
 done
 
@@ -78,13 +86,13 @@ if [ "${1}" == "push" ]; then
 
     for router in ${routers}; do
         echo "Checking for ${router}"
-        bird -c /opt/router-staging/${router}/bird.conf -p
-        bird6 -c /opt/router-staging/${router}/bird6.conf -p
+        bird -c ${STAGEDIR}/${router}/bird.conf -p
+        bird6 -c ${STAGEDIR}/${router}/bird6.conf -p
     done
 
     for router in ${routers}; do
         echo uploading for ${router}
-        rsync -avH --delete /opt/router-staging/${router}/ root@${router}:/etc/bird/
+        rsync -avH --delete ${STAGEDIR}/${router}/ root@${router}:/etc/bird/
         ssh root@${router} 'chown -R root: /etc/bird; /usr/sbin/birdc configure; /usr/sbin/birdc6 configure' | sed "s/^/${router}: /"
     done
 
@@ -100,8 +108,8 @@ elif [ "${1}" == "check" ]; then
 
     for router in ${routers}; do
         echo "Checking for ${router}"
-        bird -c /opt/router-staging/${router}/bird.conf -p
-        bird6 -c /opt/router-staging/${router}/bird6.conf -p
+        bird -c ${STAGEDIR}/${router}/bird.conf -p
+        bird6 -c ${STAGEDIR}/${router}/bird6.conf -p
     done
 else
     echo "Command '${1}' not supported" 1>&2
